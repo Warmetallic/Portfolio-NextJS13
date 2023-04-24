0d3e920936330635d0c1305090aade3c68fcf228
Gleb Kharlamov <scream10@list.ru>
2023-04-24 20:23:24 +0300

    added animation typing

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -7,9 +7,10 @@ function MenuItem({ title, url }) {
   return (
     <Link
       href={url}
-      className={`text-base lg:text-lg xl:text-xl font-semibold hover:text-red-500 ${
-        isHome ? "hidden sm:block" : ""
-      }`}
+      className={
+        `text-base lg:text-lg xl:text-xl font-semibold hover:text-red-500 ` +
+        `${isHome ? "hidden sm:block" : ""} `
+      }
     >
       <div>{title}</div>
     </Link>
---
+++ src/components/Typer.jsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+import ReactTypingEffect from "react-typing-effect";
+
+function Typer() {
+  return (
+    <div>
+      <ReactTypingEffect text={["Hello.", "World!"]} />
+    </div>
+  );
+}
+
+export default Typer;
