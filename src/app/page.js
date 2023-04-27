f0740bc824c3b8b4c3b75dd77c22a8b54f388d67
Gleb Kharlamov <scream10@list.ru>
2023-04-27 10:53:50 +0300

    adjusting main page for mobile

--- src/app/page.js
+++ src/app/page.js
@@ -9,18 +9,16 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-44">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 max-w-6xl mx-auto justify-center items-center text-sm font-mono">
-        <div className="">
+        <div className="min-w-max">
           <Typer />
           <p>I am about to play some bungo</p>
         </div>
 
-        <div className="sm:w-64 sm:h-64 lg:w-80 lg:h-80 border-2 border-red-300 rounded-lg overflow-hidden shadow-lg shadow-red-700">
+        <div className="min-w-max sm:w-64 sm:h-64 lg:w-80 lg:h-80 border-2 border-red-300 rounded-lg overflow-hidden shadow-lg shadow-red-700">
           <div className="relative h-full">
             <Canvas camera={{ fov: 25, position: [0, 0, 5] }}>
               <PhotoFrame className="w-full h-full" />
             </Canvas>
-            <div className="absolute top-0 left-0 w-full h-full border-2 border-gray-300"></div>
-            <div className="absolute bottom-0 right-0 w-full h-full border-2 border-gray-300"></div>
           </div>
         </div>
       </div>
