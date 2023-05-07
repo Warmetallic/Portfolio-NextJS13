88b3ba5420be39b1d004cf8926dbf7aed9151c0e
Gleb Kharlamov <scream10@list.ru>
2023-05-07 05:37:19 +0300

    bot fixes

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,35 +1,32 @@
-"use client";
-import React, { useEffect } from "react";
+import Script from "next/script";
 
 function ChatBot() {
-  const initBot = () => {
-    window.botpressWebChat.init({
-      botId: "0ae85e8a-aaf7-41bb-92a6-21e178d21082",
-      hostUrl: "https://cdn.botpress.cloud/webchat/v0",
-      messagingUrl: "https://messaging.botpress.cloud",
-      clientId: "0ae85e8a-aaf7-41bb-92a6-21e178d21082",
-      botName: "Red Queen AI",
-      avatarUrl:
-        "https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png",
-      botConversationDescription: "AI Powered by Umbrella Corporation",
-      showBotInfoPage: false,
-      enableConversationDeletion: false,
-      stylesheet:
-        "https://raw.githubusercontent.com/Warmetallic/botStyle/main/botStyle.css",
-    });
-  };
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://cdn.botpress.cloud/webchat/v0/inject.js";
-    script.async = true;
-    script.onload = initBot;
-    document.body.appendChild(script);
-  }, []);
-
   return (
-    <div className="z-50">
-      <div id="botpress-webchat"></div>
+    <div className="bg-red-300 dark:bg-red-300">
+      <Script
+        src="https://cdn.botpress.cloud/webchat/v0/inject.js"
+        strategy="beforeInteractive"
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.botpressWebChat.init({
+              "botId": "0ae85e8a-aaf7-41bb-92a6-21e178d21082",
+              "hostUrl": "https://cdn.botpress.cloud/webchat/v0",
+              "messagingUrl": "https://messaging.botpress.cloud",
+              "clientId": "0ae85e8a-aaf7-41bb-92a6-21e178d21082",
+              "botName": "Red Queen AI",
+              "avatarUrl": "https://bot-style.vercel.app/umbrella.png",
+              "botConversationDescription": "AI Powered by Umbrella Corporation",
+              "showBotInfoPage": false,
+              "enableConversationDeletion": true,
+              "stylesheet": "https://bot-style.vercel.app/botStyle.css"
+            });
+          `,
+        }}
+      />
+      {/* The rest of your page content */}
     </div>
   );
 }
